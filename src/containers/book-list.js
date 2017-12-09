7b5fe7d5ab6259826a3b58874524754bd6e7d5ea
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-12-09 15:07:43 -0800

    initial setup and starter functionality connecting react to redux

---
+++ src/containers/book-list.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectBook } from '../actions/index';
+
+class Booklist extends Component {
+    
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            );
+        });
+    }
+    
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) { // take application state as arguments 
+    // and whatever is return will show up as props inside of BookList
+    return { // whatever object returns from here is used as props above
+        books: state.books
+    };
+}
+
+// Anything returned from this function will end up as props 
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+    // Whenever selectBook is called, the result should be pass
+    // to all of our reducers
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know
+//about this new dispatch method, selectBook. Make it available as prop
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist);
+// connect takes a function and a component and produces a container
+
+//Container - whenever we forge a connection between a component and redux it is a Container
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+
+export default function() {
+    return [
+        { title: 'Javscript: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ];
+}
+
+
